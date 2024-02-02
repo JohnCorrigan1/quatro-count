140507dc5354253fb5664a919e168c248d59eb39
John Corrigan <jcorrigan322@gmail.com>
2024-02-01 18:42:13 -0800

    hooked up to db

--- server/server.py
+++ server/server.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import os
+# load_dotenv()
+from supabase import create_client, Client
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=['http://localhost:8081', 'http://localhost:8080', 'http://127.0.0.1:5000'])
 
 users = [
     {
@@ -96,25 +102,37 @@ groups = [
 def hello_world():
     return {"message": "Hello, World!"}
 
-@app.route("/api/users")
-def get_users():
-    return {"users": users}
+# @app.route("/api/users")
+# def get_users():
+#     return {"users": users}
+
+@app.route("/api/users", methods=['POST'] )
+@cross_origin()
+def create_user():
+    username = request.get_json().get('username', '')
+    result, count = supabase.table('users').insert({"username": username}).execute()
+    print(result)
+    return {"message": "User created", "id": result[1][0]['id'], "username": result[1][0]['username']} 
 
 @app.route("/api/users/<int:user_id>")
+@cross_origin()
 def get_user(user_id):
-    user = next((user for user in users if user["id"] == user_id), None)
-    if user is None:
-        return {"message": "User not found"}, 404
-    usermetadata = {
-        "name": user["name"],
-        "groups": []
-    }
-    #get groups for user
-    for group in groups:
-        if user_id in [member["id"] for member in group["members"]]:
-            usermetadata["groups"].append({"id": group["id"], "name": group["name"], "description": group["description"]})
+    data, count = supabase.table('users').select("username, groups").eq('id', user_id).execute()
+    response = { "name": data[1][0]["username"], "groups": []}
+    for i in data[1][0]["groups"]:
+        group, _count = supabase.table('groups').select("group_name", "description").eq('id', i).execute()
+        response["groups"].append({"id": i, "name": group[1][0]["group_name"], "description": group[1][0]["description"]})
+    return response
 
-    return usermetadata
+@app.route("/api/groups", methods=['POST'])
+@cross_origin()
+def create_group():
+    group_name = request.get_json().get('groupName', '')
+    group_description = request.get_json().get('groupDescription', '')
+    user_id = request.get_json().get('userId', '')
+    result, count = supabase.table('groups').insert({"name": group_name, "description": group_description, "members": [user_id]}).execute()
+
+
 
 @app.route("/api/users/<user_id>/groups")
 def get_user_groups(user_id):
